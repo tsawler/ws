2211554d9191ec53eb140dea1325ac2affdea810
Trevor Sawler <trevor.sawler@gmail.com>
2024-04-25 17:26:40 -0300

    tests

--- setup_test.go
+++ setup_test.go
@@ -1,22 +1,10 @@
 package ws
 
 import (
-	"github.com/go-chi/chi/v5"
-	"net/http"
 	"os"
 	"testing"
 )
 
-func testRoutes() http.Handler {
-	mux := chi.NewRouter()
-	mux.Get("/ws", func(w http.ResponseWriter, r *http.Request) {
-
-	})
-
-	return mux
-}
-
 func TestMain(m *testing.M) {
-
 	os.Exit(m.Run())
 }
