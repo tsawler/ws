fbc66ce6a471c861364c70c7497138857f07c241
Trevor Sawler <trevor.sawler@gmail.com>
2024-04-25 16:25:53 -0300

    cleanup

--- sockets.go
+++ sockets.go
@@ -7,18 +7,22 @@ import (
 	"net/http"
 )
 
-type Sockets struct{}
+// Sockets is the main type for this library.
+type Sockets struct {
+	ClientChan chan WsPayload
+	Clients    map[WebSocketConnection]string
+}
 
 // New is a factory function to return a new *Sockets object.
 func New() *Sockets {
-	return &Sockets{}
+	return &Sockets{
+		ClientChan: make(chan WsPayload),                 // The channel we send ws payloads (from client) to.
+		Clients:    make(map[WebSocketConnection]string), // A map of all connected clients.
+	}
 }
 
-var wsChan = make(chan WsPayload)                  // The channel we send ws payloads (from client) to.
-var Clients = make(map[WebSocketConnection]string) // A map of all connected Clients.
-
-// UpgradeConnection is the upgraded connection needed for ws.
-var UpgradeConnection = websocket.Upgrader{
+// upgradeConnection is the upgraded connection needed for ws.
+var upgradeConnection = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 	CheckOrigin:     func(r *http.Request) bool { return true },
@@ -43,7 +47,7 @@ type WebSocketConnection struct {
 
 // SocketEndPoint handles websocket connections
 func (s *Sockets) SocketEndPoint(w http.ResponseWriter, r *http.Request) {
-	ws, err := UpgradeConnection.Upgrade(w, r, nil)
+	ws, err := upgradeConnection.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 	}
@@ -53,14 +57,14 @@ func (s *Sockets) SocketEndPoint(w http.ResponseWriter, r *http.Request) {
 	// Create a WebSocketConnection object with the client's connection.
 	conn := WebSocketConnection{Conn: ws}
 	// Add the client to the map of connected Clients.
-	Clients[conn] = ""
+	s.Clients[conn] = ""
 
 	// Start listening for this client.
-	go listenForWS(&conn)
+	go s.listenForWS(&conn)
 }
 
 // listenForWS is the goroutine that listens for communication from Clients.
-func listenForWS(conn *WebSocketConnection) {
+func (s *Sockets) listenForWS(conn *WebSocketConnection) {
 	// If this dies, just restart it.
 	defer func() {
 		if r := recover(); r != nil {
@@ -77,31 +81,31 @@ func listenForWS(conn *WebSocketConnection) {
 		if err != nil {
 			// do nothing
 		} else {
-			// Send the incoming payload to wsChan.
+			// Send the incoming payload to SocketsChan.
 			payload.Conn = *conn
-			wsChan <- payload
+			s.ClientChan <- payload
 		}
 	}
 }
 
-// ListenToWsChannel listens to the wsChan and pushes data to broadcast function.
+// ListenToWsChannel listens to the SocketsChan and pushes data to broadcast function.
 func (s *Sockets) ListenToWsChannel() {
 	var response WsJsonResponse
 	for {
-		e := <-wsChan
+		e := <-s.ClientChan
 		response.Message = e.Message
-		BroadcastToAll(response)
+		s.BroadcastToAll(response)
 	}
 }
 
 // BroadcastToAll broadcasts data to all connected Clients.
-func BroadcastToAll(response WsJsonResponse) {
-	for client := range Clients {
+func (s *Sockets) BroadcastToAll(response WsJsonResponse) {
+	for client := range s.Clients {
 		// broadcast to every connected client
 		err := client.WriteJSON(response)
 		if err != nil {
 			_ = client.Close()
-			delete(Clients, client)
+			delete(s.Clients, client)
 		}
 	}
 }
