f3b2c5e3f5b66682df09264ef8ef1bd720e5e338
Trevor Sawler <trevor.sawler@gmail.com>
2024-04-25 17:21:25 -0300

    tests

--- sockets_test.go
+++ sockets_test.go
@@ -22,28 +22,73 @@ func Test_New(t *testing.T) {
 func TestWebSocketConnection(t *testing.T) {
 	testWS := New()
 
-	// Create test server with the echo handler.
+	// Create test serverl
 	s := httptest.NewServer(http.HandlerFunc(testWS.SocketEndPoint))
 	defer s.Close()
 
 	// Convert http://127.0.0.1 to ws://127.0.0.
 	u := "ws" + strings.TrimPrefix(s.URL, "http")
 
-	// Connect to the server
+	// Connect to the server.
 	ws, _, err := websocket.DefaultDialer.Dial(u, nil)
 	if err != nil {
 		t.Fatalf("%v", err)
 	}
 	defer ws.Close()
 
+	// Wait for our connected client to show up as a map entry in testWs.Clients.
 	for {
 		if len(testWS.Clients) > 0 {
 			break
 		}
 	}
 
+	// Create a payload.
 	payload := WsJsonResponse{
 		Message: "hi",
 	}
+
+	// Broadcast it.
 	testWS.BroadcastToAll(payload)
+
+	_, _, err = ws.ReadMessage()
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+}
+
+func Test_ListenToWsChannel(t *testing.T) {
+	testWS := New()
+
+	// Create test serverl
+	s := httptest.NewServer(http.HandlerFunc(testWS.SocketEndPoint))
+	defer s.Close()
+
+	// Convert http://127.0.0.1 to ws://127.0.0.
+	u := "ws" + strings.TrimPrefix(s.URL, "http")
+
+	// Connect to the server.
+	ws, _, err := websocket.DefaultDialer.Dial(u, nil)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	defer ws.Close()
+
+	// Wait for our connected client to show up as a map entry in testWs.Clients.
+	for {
+		if len(testWS.Clients) > 0 {
+			break
+		}
+	}
+
+	// fire off
+	go testWS.ListenToWsChannel()
+
+	for k, _ := range testWS.Clients {
+		payload := WsPayload{
+			Message: "Hello",
+			Conn:    k,
+		}
+		testWS.ClientChan <- payload
+	}
 }
